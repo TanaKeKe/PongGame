41c15f5c2a8ff797c18b05bf62eb4c8300e1ea6e
TanaKeKe <phanthanhtan2004@gmail.com>
2024-08-01 20:40:07 +0700

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private GameObject rightWall;
+    [SerializeField] private GameObject leftWall;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+
+        }
+
+        if (collision.gameObject.CompareTag("Score"))
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene(0);
+        }
+    }
+
+
+}
