3d83bfd97a958619c93282612a235bd8e4e19cb1
TanaKeKe <phanthanhtan2004@gmail.com>
2024-08-03 09:56:07 +0700

    add: Pause and Resume Game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public void Play()
+    [SerializeField] private GameObject resume;
+    [SerializeField] private GameObject pause;
+    public void Pause()
     {
         Time.timeScale = 0f;
+        pause.SetActive(false);
+        resume.SetActive(true);
     }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        pause.SetActive(true);
+        resume.SetActive(false);
+    }
+    
 }
